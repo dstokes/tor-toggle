fff7b8e4982358992da5c595018e7f0a3843d40b
Drew Stokes <drew@drewstokes.com>
2013-11-02 20:48:23 -0700

    add browser action title

--- torcheck.js
+++ torcheck.js
@@ -8,16 +8,25 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.create({ url: url });
 });
 
-(function check() {
+function updateIcon(connected) {
+  var image = 'not_connected.png'
+    , title = 'You are not connected to the tor network';
+
+  if(connected) {
+    image = 'connected.png';
+    title = 'You are connected to the tor network';
+  }
+
+  chrome.browserAction.setTitle({ title: title });
+  chrome.browserAction.setIcon({ path: { 38: '/icons/' + image } });
+}
+
+function check() {
   var xhr = new XMLHttpRequest();
   var onStateChange = function() {
     var stat = 'not_connected';
     if(xhr.readyState === 4) {
-      if(   xhr.status === 200
-         && xhr.responseText.indexOf('Sorry') === -1) {
-        stat = 'connected';
-      }
-      chrome.browserAction.setIcon({ path: { 38: '/icons/' + stat + '.png' } });
+      updateIcon(xhr.responseText.indexOf('Sorry') === -1);
     }
     checker = setTimeout(check, 50000);
   };
@@ -25,4 +34,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   xhr.onreadystatechange = onStateChange;
   xhr.open("GET", url);
   xhr.send();
-})();
+}
+
+check();
