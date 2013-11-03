ec47efa59e02ec505146ab58c36d7cb6b03e74b7
Drew Stokes <drew@drewstokes.com>
2013-11-02 23:07:19 -0700

    add proxy toggling from browser action for much awesomeness

--- torcheck.js
+++ torcheck.js
@@ -1,5 +1,11 @@
 var url = "https://check.torproject.org"
-  , checker = null;
+  , proxied = false;
+
+// define settings for tor proxy
+var proxy = {
+  mode: 'fixed_servers',
+  rules: { singleProxy: { scheme: 'socks5', host: '127.0.0.1', port: 9050 } }
+};
 
 // update the icon on network events
 window.addEventListener('online', check);
@@ -9,14 +15,18 @@ window.addEventListener('offline', function() {
 
 // listen for icon clicks
 chrome.browserAction.onClicked.addListener(function(tab) {
-  check();
-  chrome.tabs.create({ url: url });
+  var config = proxied ? { mode: 'system' } : proxy;
+  chrome.proxy.settings.set({ value: config, scope: 'regular' }, function() {
+    proxied = !proxied; 
+    updateIcon({ connected: proxied });
+  });
 });
 
 function updateIcon(options) {
   var image = 'not_connected.png'
     , title = 'You are not connected to the tor network';
 
+  proxied = !!options.connected;
   if(options.connected) {
     image = 'connected.png';
     title = 'You are connected to the tor network';
@@ -30,12 +40,10 @@ function onReadyStateChange() {
   if(this.readyState === 4) {
     var resp = this.responseText;
     updateIcon({ connected: resp && resp.indexOf('Sorry') === -1 });
-    checker = setTimeout(check, 100000);
   }
 }
 
 function check() {
-  clearTimeout(checker);
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = onReadyStateChange;
   xhr.open("GET", url);
