9c30fb8328bd2a7884d7ed8c13312efd2c1b2beb
Drew Stokes <drew@drewstokes.com>
2013-11-03 10:37:11 -0800

    gracefully handle status check failures

--- index.js
+++ index.js
@@ -1,53 +1,99 @@
 var url = "https://check.torproject.org"
   , proxied = false;
 
-// define settings for tor proxy
-var proxy = {
+/**
+ * Define network settings
+ */
+var system_network = { mode: 'system' };
+var tor_network = {
   mode: 'fixed_servers',
   rules: { singleProxy: { scheme: 'socks5', host: '127.0.0.1', port: 9050 } }
 };
 
-// update the icon on network events
-window.addEventListener('online', check);
+/**
+ * Check tor connection status when coming online
+ */
+window.addEventListener('online', function() {
+  checkProxy(onProxyCheck);
+});
+
+/**
+ * Update browser action icon when going offline
+ */
 window.addEventListener('offline', function() {
-  updateIcon({ connected: false });
+  onConnectionChange(false);
 });
 
-// listen for icon clicks
+// watch for proxy errors
+/**
+ * Update browser action icon and proxy settings on proxy error
+ */
+chrome.proxy.onProxyError.addListener(function(details) {
+  console.error('Proxy Error: ' + details.error);
+  console.info('Make sure tor is running at localhost:9050');
+
+  // disable proxy
+  toggleTorProxy(onConnectionChange);
+});
+
+/**
+ * Toggle tor proxy settings on browser action icon click
+ */
 chrome.browserAction.onClicked.addListener(function(tab) {
-  var config = proxied ? { mode: 'system' } : proxy;
-  chrome.proxy.settings.set({ value: config, scope: 'regular' }, function() {
-    proxied = !proxied; 
-    updateIcon({ connected: proxied });
-  });
+  toggleTorProxy(onConnectionChange);
 });
 
-function updateIcon(options) {
-  var image = 'not_connected.png'
-    , title = 'You are not connected to the tor network';
+/**
+ * Toggle chrome proxy settings between system and tor
+ */
+function toggleTorProxy(cb) {
+  var config = proxied ? system_network : tor_network;
+  chrome.proxy.settings.set({ value: config, scope: 'regular' }, function() {
+    cb(proxied = !proxied);
+  });
+}
 
-  proxied = !!options.connected;
-  if(options.connected) {
-    image = 'connected.png';
-    title = 'You are connected to the tor network';
-  }
+/**
+ * Update the browser action icon
+ */
+function onConnectionChange(connected) {
+  var stat  = connected ? "are" : "aren't"
+    , image = connected ? 'connected.png' : 'not_connected.png'
+    , title = "You "+ stat +" connected to the tor network";
 
   chrome.browserAction.setTitle({ title: title });
   chrome.browserAction.setIcon({ path: { 38: '/icons/' + image } });
 }
 
-function onReadyStateChange() {
-  if(this.readyState === 4) {
-    var resp = this.responseText;
-    updateIcon({ connected: resp && resp.indexOf('Sorry') === -1 });
-  }
-}
-
-function check() {
+/**
+ * Check for a current tor connection using check.torproject.org
+ */
+function checkProxy(cb) {
   var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = onReadyStateChange;
+  // don't wait too long
+  xhr.timeout = 5000;
+  xhr.onerror = cb;
+  xhr.ontimeout = cb;
+  xhr.onload = function(e) {
+    var resp = e.target.responseText;
+    cb(null, (resp && resp.indexOf('Sorry') === -1));
+  };
   xhr.open("GET", url);
   xhr.send();
 }
 
-check();
+/**
+ * Handle proxy connection status check
+ */
+function onProxyCheck(err, isTor) {
+  if(err) {
+    console.warn('Failed to check tor status at ' + url);
+    return chrome.browserAction.setTitle({
+      title: 'Unable to check tor status'
+    });
+  }
+  onConnectionChange(isTor);
+}
+
+// check proxy status on boot
+checkProxy(onProxyCheck);
